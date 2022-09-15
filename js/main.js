f305a09e1304ac3487b81675ad0e25d06ffe269b
Вера <Vera.Iagodkina@urfu.me>
2022-09-15 15:17:46 +0500

    исправлен слеш и функция стала короче

--- js/main.js
+++ js/main.js
@@ -4,15 +4,10 @@ function getRandomArbitrary(min,max){
     min = max;
     max = a;
   }
-  if (min < 0){
-    min = 0;
-  }
-  if (min === max) {
-    return Math.round(min);
-  }
+  min = min < 0 ? 0 : min;
+  max = max < 0 ? 0 : max;
   return Math.round(Math.random()*(max-min) + min); //формула с сайта MDN
 }
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
-// eslint-disable-next-line no-console
-console.log(getRandomArbitrary(9,8));
+getRandomArbitrary(9,8);
 checkStringLength('str',5);
