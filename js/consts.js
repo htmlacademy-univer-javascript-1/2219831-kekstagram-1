8164a96cc48954b3975e8eb770f24b40f2b699a9
Вера <Vera.Iagodkina@urfu.me>
2022-11-29 19:54:43 +0500

    добавлен слайдер и эффекты

--- js/consts.js
+++ js/consts.js
@@ -30,4 +30,32 @@ const CountComment = {
 const STEP_ADDED_COMMENTS = 5;
 const DEFAULT_RENDERED_COMMENTS = 5;
 
-export{MAX_COUNT_PHOTOS, CountLike, MAX_COMMENT_LENGTH, MaxHashtag, ErrorMessage, STEP_ADDED_COMMENTS, DEFAULT_RENDERED_COMMENTS, CountComment};
+const Effects = {
+  'none': { name: 'none', filter: '', unit: '',
+    options: {range: {min: 0, max: 100}, step: 1, start: 100},
+  },
+  'chrome': {name: 'chrome', filter: 'grayscale', unit: '',
+    options: { range: {min: 0, max: 1}, step: 0.1, start: 1},
+  },
+  'sepia': { name: 'sepia', filter: 'sepia', unit: '',
+    options: { range: {min: 0, max: 1}, step: 0.1, start: 1},
+  },
+  'marvin': { name: 'marvin', filter: 'invert', unit: '%',
+    options: {range: {min: 0, max: 100}, step: 1, start: 100},
+  },
+  'phobos': {name: 'phobos', filter: 'blur', unit: 'px',
+    options: {range: {min: 0, max: 3}, step: 0.1, start: 3},
+  },
+  'heat': {name: 'heat', filter: 'brightness', unit: '',
+    options: {range: {min: 1, max: 3}, step: 0.1, start: 3},
+  }
+};
+
+const SLIDER_STEP = 25;
+const ScaleRange = {
+  min: 25,
+  max: 100
+};
+
+export {MAX_COUNT_PHOTOS, CountLike, MAX_COMMENT_LENGTH, MaxHashtag, ErrorMessage, STEP_ADDED_COMMENTS,
+  DEFAULT_RENDERED_COMMENTS, CountComment, Effects, SLIDER_STEP, ScaleRange};
---
+++ js/effects.js
@@ -0,0 +1,63 @@
+import { Effects, SLIDER_STEP, ScaleRange } from './consts.js';
+
+const imageForChange = document.querySelector('.img-upload__preview').querySelector('img');
+const slider = document.querySelector('.effect-level__slider');
+const sliderValue = document.querySelector('.effect-level__value');
+const sliderWrapper = document.querySelector('.img-upload__effect-level');
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const scaleControlValue = document.querySelector('.scale__control--value');
+
+const checkScaleClicks = (val) => {
+  if (val > ScaleRange.max) {
+    val = ScaleRange.max;
+  }
+  if (val <= ScaleRange.min){
+    val = ScaleRange.min;
+  }
+  return val;
+};
+
+const changeScale = (val) => {
+  const sliderNumber = checkScaleClicks(Number(scaleControlValue.value.replace('%', '')) + SLIDER_STEP * val);
+  imageForChange.style.transform = `scale(${sliderNumber / 100})`;
+  scaleControlValue.value = `${sliderNumber}%`;
+};
+
+const onScaleButtonClick = () => {
+  scaleControlBigger.addEventListener('click', () => changeScale(1));
+  scaleControlSmaller.addEventListener('click', () => changeScale(-1));
+};
+
+const createSlider = () => {
+  sliderWrapper.classList.add('hidden');
+  noUiSlider.create(slider, {
+    range: {min: 0, max: 100}, start: 100, step: 0.1,
+    format: {
+      to: (value) => (Number.isInteger(value)) ? value.toFixed(0) : value.toFixed(1),
+      from: (value) => parseFloat(value),
+    },
+  });
+};
+
+const updateSliderSettings = (evt) => {
+  const effect = evt.target.value;
+  if (effect === 'none') {
+    sliderWrapper.classList.add('hidden');
+    imageForChange.style.filter = 'none';
+    return;
+  }
+
+  sliderWrapper.classList.remove('hidden');
+  imageForChange.removeAttribute('class');
+  imageForChange.classList.add(`effects__preview--${effect}`);
+  slider.noUiSlider.updateOptions(Effects[effect].options);
+
+  slider.noUiSlider.on('update', () => {
+    sliderValue.value = slider.noUiSlider.get();
+    imageForChange.style.filter = `${Effects[effect].filter}(${sliderValue.value}${Effects[effect].unit})`;
+  });
+};
+
+
+export {updateSliderSettings, createSlider, onScaleButtonClick};
